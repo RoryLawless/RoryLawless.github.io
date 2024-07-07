dc18dd86957bc32d04fd9d117b358b3bb924dee1
Rory <24260923+RoryLawless@users.noreply.github.com>
2024-07-07 11:35:05 -0400

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Rory Lawless",
+	url: "https://rorylawless.github.io",
+	language: "en",
+	description: "The personal website of Rory Lawless",
+	author: {
+		name: "Rory Lawless",
+		email: "rory@rorylawless.com",
+		url: "https://rorylawless.github.io/about-me/"
+	}
+}
